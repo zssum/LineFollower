3fe61535f068ddc71b4196f74899a1c742e1f0d7
zssum <sumzhanseng@hotmail.com>
2017-05-09 10:01:39 +0000

    solving issue of cornering

--- robo/robo.ino
+++ robo/robo.ino
@@ -12,17 +12,18 @@
 #define RM_PWM  3
 #define MOTORSPEED  50
 
-#define Kp 0 // experiment to determine this, start by something small that just makes your bot follow the line at a slow speed
-#define Kd 0 // experiment to determine this, slowly increase the speeds and adjust this value. ( Note: Kp < Kd) 
-#define RIGHT_MAX_SPEED 200 // max speed of the robot
-#define LEFT_MAX_SPEED 200 // max speed of the robot
-#define RIGHT_BASE_SPEED 150 // this is the speed at which the motors should spin when the robot is perfectly on the line
-#define LEFT_BASE_SPEED 150  // this is the speed at which the motors should spin when the robot is perfectly on the line
+#define Kp 0.008 // experiment to determine this, start by something small that just makes your bot follow the line at a slow speed
+#define Kd 0.01 // experiment to determine this, slowly increase the speeds and adjust this value. ( Note: Kp < Kd) 
+#define RIGHT_MAX_SPEED 120 // max speed of the robot
+#define LEFT_MAX_SPEED  120// max speed of the robot
+#define RIGHT_BASE_SPEED 80 // this is the speed at which the motors should spin when the robot is perfectly on the line
+#define LEFT_BASE_SPEED 80  // this is the speed at which the motors should spin when the robot is perfectly on the line
 
 // sensors 0 through 7 are connected to digital pins 3 through 10, respectively
 QTRSensorsRC qtrrc((unsigned char[]) {22, 24, 26, 28, 30, 32, 34, 36},
   NUM_SENSORS, TIMEOUT, EMITTER_PIN); 
 unsigned int sensorValues[NUM_SENSORS];
+
 
 String action;
 int lastError = 0;
@@ -38,6 +39,25 @@ void setup()
   Serial.setTimeout(80);
   Serial.begin(9600); // set the data rate in bits per second for serial data transmission
   delay(1000);
+  
+  qtrrc.calibrate();
+  qtrrc.calibratedMinimumOn[0]=908;
+  qtrrc.calibratedMinimumOn[1]=596;
+  qtrrc.calibratedMinimumOn[2]=492;
+  qtrrc.calibratedMinimumOn[3]=540;
+  qtrrc.calibratedMinimumOn[4]=440;
+  qtrrc.calibratedMinimumOn[5]=440;
+  qtrrc.calibratedMinimumOn[6]=440;
+  qtrrc.calibratedMinimumOn[7]=544;
+  
+  
+  for (int i = 0; i < NUM_SENSORS; i++)
+  {
+    qtrrc.calibratedMaximumOn[i]=2500;
+  }
+  //qtrrc.calibratedMinimumOn=minimum;
+  //qtrrc.calibratedMaximumOn=maximum;
+  
 }
 
 
@@ -86,6 +106,7 @@ void read(){
 }
 
 void calibrate(){
+  
   delay(500); 
   digitalWrite(13, HIGH);    // turn on Arduino's LED to indicate we are in calibration mode
   for (int i = 0; i < 200; i++)  // make the calibration take about 5 seconds
@@ -176,28 +197,48 @@ void motorStop(){
 
 void drive(){
   unsigned int position = qtrrc.readLine(sensorValues); // get calibrated readings along with the line position, refer to the QTR Sensors Arduino Library for more details on line position.
-  int error = position - 3500;
+  int error = 3500-position;
 
   int motorSpeed = Kp * error + Kd * (error - lastError);
   lastError = error;
 
-  int rightMotorSpeed = RIGHT_BASE_SPEED + motorSpeed;
-  int leftMotorSpeed = LEFT_BASE_SPEED - motorSpeed;
+  int rightMotorSpeed = RIGHT_BASE_SPEED - motorSpeed;
+  int leftMotorSpeed = LEFT_BASE_SPEED + motorSpeed;
   
   if (rightMotorSpeed > RIGHT_MAX_SPEED ) rightMotorSpeed = RIGHT_MAX_SPEED; // prevent the motor from going beyond max speed
   if (leftMotorSpeed > LEFT_MAX_SPEED ) leftMotorSpeed = LEFT_MAX_SPEED; // prevent the motor from going beyond max speed
   if (rightMotorSpeed < 0) rightMotorSpeed = 0; // keep the motor speed positive
   if (leftMotorSpeed < 0) leftMotorSpeed = 0; // keep the motor speed positive
+  
+  if(error<-3000 ){
+    //motorStop();
+    //delay(5);
+    motorLeft(60);
+    Serial.print("lockleft");    
+    Serial.println();
+  } else if (error>3000){
+    Serial.print("lockright");
+    Serial.println();
+    //motorStop();
+    //delay(5);
+    motorRight(60);
+  } else {
+    analogWrite(LM_PWM,leftMotorSpeed);
+    digitalWrite(LM1,HIGH);
+    digitalWrite(LM2,LOW);
+    analogWrite(RM_PWM,rightMotorSpeed);
+    digitalWrite(RM1,HIGH);
+    digitalWrite(RM2,LOW);
+    //Serial.print("drive");
+    //Serial.println();
+  }
 
-  analogWrite(LM_PWM,leftMotorSpeed);
-  digitalWrite(LM1,HIGH);
-  digitalWrite(LM2,LOW);
-  analogWrite(RM_PWM,rightMotorSpeed);
-  digitalWrite(RM1,HIGH);
-  digitalWrite(RM2,LOW);
-  Serial.print("drive");
-  Serial.println();
+  
   delay(1);
 }
+
+//1052 776 632 676 576 576 580 788 
+
+//2500 2500 2500 2500 2500 2500 2500 2500 
 
 
