84054b5c5f4d89991f5e7c482d055e49ea5b6e99
zssum <sumzhanseng@hotmail.com>
2017-06-13 18:01:28 +0800

    in progress

--- robo/robo.ino
+++ robo/robo.ino
@@ -21,14 +21,14 @@
 #define LM1     33       
 #define LM2     35
 #define LM_PWM  2 
-#define L_GND   37
-#define L_5V    39
+#define L_GND   37 // TODO: check which todelete
+#define L_5V    39 // TODO: check which todelete
 #define RM1     25
 #define RM2     27
 #define RM_PWM  3
 #define R_GND   29
 #define R_5V    31
-#define ROBOT_MAX_SPEED_FACTOR  1.7 // Factor of speedSelected to allow for speed variation
+#define ROBOT_MAX_SPEED_FACTOR  1.7 // Factor of speedSelected to allow for speed variation between left and right wheels
 
 // Ultrasound Distance Detector Settings
 #define  trigPin  51    
@@ -36,28 +36,26 @@
 #define  sonicVcc 53
 #define  sonicGnd 47
 
+#define servonum  0 // tapper is attached to channel 0 out of 16 of the pwm controlboard
+
 // Initialising motors and QTR(ir) sensor and tapper
 QTRSensorsRC qtrrc((unsigned char[]) {24, 26, 28, 30, 32, 34},
   NUM_SENSORS, TIMEOUT, EMITTER_PIN); 
 Motor motor(LM1, LM2, LM_PWM, RM1, RM2, RM_PWM);
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
-uint8_t servonum = 0;// tapper is attached to channel 0 out of 16 of the pwm controlboard
 
-
-int speedSelected=40; //Inital speed 
+#define INITIAL_SPEED 40
+int speedSelected= INITIAL_SPEED; //Inital speed 
 float Kp=0.035; // LineFollower proportional constant
 unsigned int sensorValues[NUM_SENSORS]; //IR Sensor values
 
 
 long duration, cm; //distance detection variables
-
 String action; // Robot's state
 String inputString; // Command builder for serial strings
 boolean directionTogg=true; // Toggle between clockwise and anti-clockwise rotation movements as a feedback to user
 boolean isLaunchFromStop=true; // Used to soften acceleration if robot is launching from rest 
 boolean tapped=false; //tapper state
-int skipStop=0;
-int seq=0;
 
 void setup()
 {
@@ -73,24 +71,23 @@ void setup()
   pinMode(R_5V, OUTPUT);
   digitalWrite(R_GND,LOW);
   digitalWrite(R_5V,HIGH);
-  pinMode(L_GND,OUTPUT);
-  pinMode(L_5V, OUTPUT);
-  digitalWrite(R_GND,LOW);
-  digitalWrite(R_5V,HIGH);
+  pinMode(L_GND,OUTPUT); // TODO: check which todelete
+  pinMode(L_5V, OUTPUT); // TODO: check which todelete
+  digitalWrite(R_GND,LOW); // TODO: check which todelete
+  digitalWrite(R_5V,HIGH); // TODO: check which todelete
   Serial.begin(9600); // set the data rate in bits per second for serial data transmission
   Serial.setTimeout(80);
-  Serial1.begin(9600); 
+  Serial1.begin(9600);  //set bluetooth baud rate
   
   //tapper initialisation
   pwm.begin();
   pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
-  pwm.setPWM(servonum, 0, 0);
+  pwm.setPWM(servonum, 0, 0); // Turn off servo
  
  
   qtrrc.calibrate();
-  //Manual callibration by recording read() values
-  
-  // increased clearance 1104	1104	1020	1224	1264	1388		plus 1000
+  // Manual callibration by recording read() values  
+  // Raw values at KCD floor: 1104	1104	1020	1224	1264	1388		plus 1000 for noise reduction
   qtrrc.calibratedMinimumOn[0]=2104;
   qtrrc.calibratedMinimumOn[1]=2104;
   qtrrc.calibratedMinimumOn[2]=2020;
@@ -100,7 +97,7 @@ void setup()
   
   for (int i = 0; i < NUM_SENSORS; i++)
   {
-    qtrrc.calibratedMaximumOn[i]=4500;
+    qtrrc.calibratedMaximumOn[i]=TIMEOUT;
   }
   
 }
@@ -108,25 +105,22 @@ void setup()
 
 void loop()
 {
-  if(action=="read") read();
-  else if (action=="select") selectSpeed();
-  else if (action=="readline") readline();
-  else if (action=="calibrate") calibrate();
+  if(action=="read") read(); // Read raw values of line sensor
+  else if (action=="readline") readline(); // Read calibrated values and position of Robot on the line
+  else if (action=="calibrate") calibrate(); //Recalibrate line sensor to a different surface
+  else if (action=="select") selectSpeed();  
+  else if (action=="go") go(); // Switching the motors in the forward direction before moving on to the main robot program
+  else if (action=="d") drive(); // Main robot program
+  // Manual control of the movement of the robot
   else if (action=="f") motor.motorFwd(40);
   else if (action=="b") motor.motorBack(40);
-  else if (action=="l") motor.motorLeft(60);
-  else if (action=="r") motor.motorRight(60);
+  else if (action=="l") motor.motorLeft(60); // Rotate Anti-Clockwise
+  else if (action=="r") motor.motorRight(60); //Rotate Clockwise
   else if (action=="s") motor.motorStop();
-  else if (action=="go") go();
-  else if (action=="d") drive();
-  else if (action=="pose") pose(); //TODO: tapCard() 
-  else if (action=="t") toggleTapper();
-  else if (action=="tap") tapCard();
   else motor.motorStop();
   
   //Ensures robot will brake if an object is in front
-  //The robot will continue to move if the obstacle is removed  
-  
+  //The robot will continue to move if the obstacle is removed   
   detectRange(); 
   while(cm<25){
     detectRange();
@@ -137,61 +131,6 @@ void loop()
 }
 
 
-//Program to select the motor's speed from 70-110 in increments of 10
-//User presses "selectSpeed" button to start the program, then presses "+" button to increment 10 to the base speed of 70
-//Robot responds with a jerk on press of the increment button
-//speedSelecting takes values 0 to 4. A selection of 0 gives speed of 70 while a selection of 4 gives speed of 110
-//At the end of selection, user presses "selectSpeed" button to end the program. The robot will jerk the number of times to feedback the selected speed to the user.
-void selectSpeed(){
-  digitalWrite(13,HIGH); //Turn on indicators to feedback the selectSpeed program is ongoing
-  motor.motorStop();
-  boolean done=false;
-  int speedSelecting=0;
-  while (!done){
-    while (Serial1.available()) {  
-      char inChar = (char)Serial1.read();
-      if (inChar == '+') {
-        speedSelecting++;
-        jerk();
-        if(speedSelecting==5){
-          done=true;
-          digitalWrite(13,LOW);
-          delay(500);
-        }
-      } else if(inChar=='#'){
-        done=true;
-        digitalWrite(13,LOW);
-        delay(500);
-      }
-    }
-  }
-  debugln("Speed selected: ");
-  debug(speedSelecting);
-  for(int i=0;i<speedSelecting;i++){
-    jerk();
-  }
-  //adjust speed and linefollowing constants
-  speedSelected=40+speedSelecting*10; 
-  Kp= 0.035*40/speedSelected;
-  action="s";
-}
-
-//TODO: Replace with tapping method
-void pose(){
-  motor.motorStop();
-  delay(500);
-  motor.motorLeft(50);
-  delay(200);
-  motor.motorStop();
-  delay(200);
-  motor.motorRight(50);
-  delay(200);
-  motor.motorStop();
-  delay(200);
-  action="go"; 
-}
-
-// read raw sensor values
 void read(){
   qtrrc.read(sensorValues);
   // print the sensor values as numbers from 0 to 2500, where 0 means maximum reflectance and
@@ -259,6 +198,45 @@ void calibrate(){
   action= "s";
 }
 
+//Program to select the motor's speed from 70-110 in increments of 10
+//User presses "selectSpeed" button to start the program, then presses "+" button to increment 10 to the base speed of 70
+//Robot responds with a jerk on press of the increment button
+//speedSelecting takes values 0 to 4. A selection of 0 gives speed of 70 while a selection of 4 gives speed of 110
+//At the end of selection, user presses "selectSpeed" button to end the program. The robot will jerk the number of times to feedback the selected speed to the user.
+void selectSpeed(){
+  digitalWrite(13,HIGH); //Turn on indicators to feedback the selectSpeed program is ongoing
+  motor.motorStop();
+  boolean done=false;
+  int speedSelecting=0;
+  while (!done){
+    while (Serial1.available()) {  
+      char inChar = (char)Serial1.read();
+      if (inChar == '+') {
+        speedSelecting++;
+        jerk();
+        if(speedSelecting==5){
+          done=true;
+          digitalWrite(13,LOW);
+          delay(500);
+        }
+      } else if(inChar=='#'){
+        done=true;
+        digitalWrite(13,LOW);
+        delay(500);
+      }
+    }
+  }
+  debugln("Speed selected: ");
+  debug(speedSelecting);
+  for(int i=0;i<speedSelecting;i++){
+    jerk();
+  }
+  //adjust speed and linefollowing constants
+  speedSelected=INITIAL_SPEED+speedSelecting*10; 
+  Kp= 0.035*40/speedSelected;
+  action="s";
+}
+
 void go(){
   analogWrite(LM_PWM,0);
   digitalWrite(LM1,HIGH);
@@ -272,8 +250,7 @@ void go(){
 void drive(){
   unsigned int position = qtrrc.readLine(sensorValues); // get calibrated readings along with the line position, refer to the QTR Sensors Arduino Library for more details on line position.
   int error = 2500-position;
-  int motorSpeed = Kp * error + Kd * (error - lastError);
-  lastError = error;
+  int motorSpeed = Kp * error;
   int rightMotorSpeed = speedSelected - motorSpeed;
   int leftMotorSpeed = speedSelected + motorSpeed;
   
@@ -281,10 +258,9 @@ void drive(){
   if (leftMotorSpeed > ROBOT_MAX_SPEED_FACTOR*speedSelected ) leftMotorSpeed = ROBOT_MAX_SPEED_FACTOR*speedSelected; 
   if (rightMotorSpeed < 0) rightMotorSpeed = 0; // keep the motor speed positive
   if (leftMotorSpeed < 0) leftMotorSpeed = 0; 
-
   
   
-  if(error==-2500 ){ // if line is on the left of the robot, stop line detection and rotate anti-clockwise until line is at the center before moving off
+  if(error==-2500 ){ // if line is on the left of the robot, stop line detection and rotate anti-clockwise until line is at nearer to the center of the line before moving off
     while(error<-1000){
       motor.motorLeft(65);
       error = 2500-qtrrc.readLine(sensorValues);      
@@ -325,26 +301,14 @@ void drive(){
     if(sensorValues[i]>950) black++;
   }
   if(black==NUM_SENSORS) {
-    if(skipStop==0){
-      for(int i=1;i<6;i++){
-      analogWrite(LM_PWM,leftMotorSpeed/2^i);
-      analogWrite(RM_PWM,rightMotorSpeed/2^i);
-      delay(10);
-      }
-      motor.motorStop();
-      isLaunchFromStop=true;
-      action="s";
-      if(seq==2){
-        skipStop=0;
-        seq=0;
-      }else{
-        seq++;
-        skipStop=3;
-      }
-    } else{
-      delay(100);
-      skipStop--;
+    for(int i=1;i<6;i++){
+    analogWrite(LM_PWM,leftMotorSpeed/2^i);
+    analogWrite(RM_PWM,rightMotorSpeed/2^i);
+    delay(10);
     }
+    motor.motorStop();
+    isLaunchFromStop=true;
+    action="tap";
   }
   debugln(position);
   delay(1); // stabilise robot
